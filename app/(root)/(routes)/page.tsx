663771992bea8c781df7b2b67ca897a0181d937e
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-08-03 09:37:38 -0400

    created search input feature

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import SearchInput from "@/components/SearchInput";
 import { UserButton } from "@clerk/nextjs";
 
 const RootPage = () => {
   return (
-    <div>
-      
+    <div className="h-full p-4 space-y-2">
+      <SearchInput />
     </div>
   );
 };
