b5a657447a56e4ecea331d1857d0ae7d5a28b10d
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-08-04 04:51:19 -0400

    created the category selectors

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,18 @@
+import { Categories } from "@/components/Categories";
 import SearchInput from "@/components/SearchInput";
 import prismadb from "@/lib/prismadb";
 import { UserButton } from "@clerk/nextjs";
 
 const RootPage = async () => {
   const categories = await prismadb.category.findMany();
+  
+  
+  
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
+      
     </div>
   );
 };
