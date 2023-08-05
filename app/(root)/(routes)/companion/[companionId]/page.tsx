7ce8cab0f3cdd64a9ac24c73fffcd3b80af23800
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-08-05 11:10:21 -0400

    chat messages ui

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,32 +1,31 @@
 import prismadb from "@/lib/prismadb";
 import { CompanionForm } from "./components/CompanionForm";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
-interface CompanionIdPageProps 
-    {
-        params: {
-            companionId: string
-        }
-    }
-
-const CompanionIdPage = async ( {params}: CompanionIdPageProps) => {
-
-// TODO: Check subscription
-
-const companion = await prismadb.companion.findUnique({
-    where: {
-        id: params.companionId
-    }
-})
-
-const categories = await prismadb.category.findMany()
-
-    return (
-    <CompanionForm
-    initialData={companion}
-    categories={categories}
-    />
-    
-    )
+interface CompanionIdPageProps {
+  params: {
+    companionId: string;
+  };
 }
- 
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirectToSignIn();
+  }
+  // TODO: Check subscription
+
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      userId: userId,
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return <CompanionForm initialData={companion} categories={categories} />;
+};
+
 export default CompanionIdPage;
