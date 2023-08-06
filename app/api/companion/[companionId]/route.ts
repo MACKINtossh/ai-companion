a1a9dd8bad3a13793c674c97e7301f1f302e0ed6
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-08-06 09:11:01 -0400

    protecting routes

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -3,6 +3,7 @@ import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
+import { checkSubscription } from "@/lib/subscription";
 // import { checkSubscription } from "@/lib/subscription";
 
 export async function PATCH(
@@ -33,11 +34,11 @@ export async function PATCH(
       return new NextResponse("Missing required fields", { status: 400 });
     }
 
-    // const isPro = await checkSubscription();
+    const isPro = await checkSubscription();
 
-    // if (!isPro) {
-    //   return new NextResponse("Pro subscription required", { status: 403 });
-    // }
+    if (!isPro) {
+      return new NextResponse("Pro subscription required", { status: 403 });
+    }
 
     const companion = await prismadb.companion.update({
       where: {
